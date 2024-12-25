ebeb4deb952289a34cd097a5308e18fad2c5e69d
Andrzej Gac <gac.andrzej@gmail.com>
2024-12-25 23:59:03 +0100

    add readme

---
+++ app/src/main/java/gac/andrzej/mytestkot/MonteCarlo.kt
@@ -0,0 +1,20 @@
+package gac.andrzej.mytestkot
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MonteCarlo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_monte_carlo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
